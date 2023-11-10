484a989f637c418f749a229f929c514c8b7147e8
Emily Sali <emily.sali@outlook.com>
2023-11-10 17:11:47 +0000

    rename CPP1 to CPP00

---
+++ CPP00/ex01/PhoneBook.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 16:48:17 by esali             #+#    #+#             */
+/*   Updated: 2023/11/10 17:00:01 by esali            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include <iostream>
+
+class PhoneBook {
+
+
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+};
+
+#endif
