938ec89442a3ba9eb7622ffd416392f270a4888f
emSali <emily.sali@outlook.com>
2023-11-24 12:22:39 +0000

    update main of C02/ex03

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,26 +6,18 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:35:38 by esali             #+#    #+#             */
-/*   Updated: 2023/11/23 22:59:31 by esali            ###   ########.fr       */
+/*   Updated: 2023/11/24 12:21:52 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
+#include "Point.hpp"
 
 int main(void)
 {
-	Fixed a;
-	Fixed const b(Fixed(5.05f) * Fixed(2));
+	Point const a(0, 0);
+	Point const b(0, 5);
+	Point const c(5, 0);
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-
-	std::cout << b << std::endl;
-
-	std::cout << Fixed::max(a, b) << std::endl;
 
 	return (0);
 }
