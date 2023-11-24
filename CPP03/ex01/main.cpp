8862decc563b26996cf3d7689cd35603bf8dc3e4
emSali <emily.sali@outlook.com>
2023-11-24 16:39:09 +0000

    C02/ex01

---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 13:02:37 by esali             #+#    #+#             */
+/*   Updated: 2023/11/24 16:30:40 by esali            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int main( void ) {
+
+
+	ScavTrap scav("Mario");
+	scav.guardGate();
+	scav.attack("Bowser");
+	scav.takeDamage(50);
+	scav.beRepaired(30);
+	std::cout << std::endl;
+
+	ScavTrap scav2(ScavTrap ("Peach"));
+	scav2.guardGate();
+	scav2.attack("Bowser");
+	scav2.takeDamage(50);
+	scav2.beRepaired(30);
+	std::cout << std::endl;
+
+	ScavTrap scav3 = ScavTrap ("Luigi");
+	scav3.guardGate();
+	scav3.attack("Mario");
+	scav3.takeDamage(50);
+	scav3.beRepaired(30);
+	std::cout << std::endl;
+
+
+}
