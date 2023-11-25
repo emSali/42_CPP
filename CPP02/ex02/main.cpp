c998da39889fce38cf906bb2c2948af6f3d74c96
Emily Sali <emily.sali@outlook.com>
2023-11-25 16:01:49 +0000

    update C02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esali <esali@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 21:35:38 by esali             #+#    #+#             */
-/*   Updated: 2023/11/23 22:59:31 by esali            ###   ########.fr       */
+/*   Updated: 2023/11/25 16:00:07 by esali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,59 @@ int main(void)
 	std::cout << b << std::endl;
 
 	std::cout << Fixed::max(a, b) << std::endl;
+
+
+	std::cout << std::endl << std::endl;
+	if (a > b)
+		std::cout << "a is bigger than b" << std::endl;
+	else if (a < b)
+		std::cout << "a is smaller than b" << std::endl;
+	else if (a == b)
+		std::cout << "a and b are equal" << std::endl;
+
+	if (a != b)
+		std::cout << "a and b are not equal" << std::endl;
+
+	if (a <= b)
+		std::cout << "a is equal or smaller b" << std::endl;
+	else if (a >= b)
+		std::cout << "a is equal or bigger than b" << std::endl;
+
+
+	Fixed c = a + b;
+	std::cout << "Value of c after addition: of " << a << " and " << b << ": " << c << std::endl;
+	if (c <= a)
+		std::cout << "c is smaller than a" << std::endl;
+	else if (c <= b)
+		std::cout << "c is smaller than b and bigger than a" << std::endl;
+	else
+		std::cout << "c is bigger than a and b" << std::endl;
+
+	c = c - a;
+	std::cout << "Value of c after subtraction of " << a << ": " << c << std::endl;
+	if (c != b)
+		std::cout << "c is not equal to a" << std::endl;
+	else if (c == b)
+		std::cout << "c is equal to a" << std::endl;
+
+	std::cout << "Value of c before mulitplication with 10: " << c << std::endl;
+	c =  c * Fixed(10);
+	std::cout << "Value of c after mulitplication with : " << c << std::endl;
+	c = c / Fixed(2);
+	std::cout << "Value of c during post-decrement: " << c-- << std::endl;
+	std::cout << "Value of c after post-decrement: " << c-- << std::endl;
+
+	std::cout << "Value of c during pre-decrement " << c << std::endl;
+
+
+
+
+
+
+
+
+
+
 
 	return (0);
 }
