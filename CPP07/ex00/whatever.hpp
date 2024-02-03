a402158fcc57a663358dccdb1fdabd7a7e9bfe51
emSali <emily.sali@outlook.com>
2024-02-03 11:48:30 +0000

    CPP07/ex00

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -4,6 +4,25 @@
 #include <iostream>
 #include <string>
 
+template<typename T>
+void swap(T &x, T &y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
 
+template <typename T>
+T const & min(T const &x, T const &y) {
+	if (x < y)
+		return x;
+	return y;
+}
+
+template <typename T>
+T const & max(T const &x, T const &y) {
+	if (x > y)
+		return x;
+	return y;
+}
 
 #endif
